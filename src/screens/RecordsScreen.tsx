f547b7d7e9748bb53fb1b80241a43e6d9270271d
junho <junho@user.com>
2026-04-14 13:43:51 +0000

    fix: replace uuid with inline generator for RN compatibility

--- src/screens/RecordsScreen.tsx
+++ src/screens/RecordsScreen.tsx
@@ -11,7 +11,13 @@ import {
   TextInput,
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import {v4 as uuidv4} from 'uuid';
+function uuidv4(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 import {TravelRecord} from '../types';
 import {loadRecords, addRecord, updateRecord, deleteRecord} from '../storage/store';
 import {useFocusEffect} from '@react-navigation/native';
